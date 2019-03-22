8125f2c0d029ab5ae63b32f0e64cce0dbcca3e8c
Nemes <nemesgyadam@gmail.com>
2019-03-22 11:32:24 +0000

    Added run_on_testnet.sh

---
+++ bitcoin/run_on_testnet.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker run -v /home/ubuntu/bitcoin:/home/ubuntu/bitcoin --rm -it -d -p 18332:18332 nemesgyadam/bitcoin-core -testnet=1 -rpcallowip=172.17.0.0/16 -rpcauth='nemes:762008f3357fb7ed19864d632f62701e$19685dea44164627fd3277feeac445e2ef52002b816441ad531b92db9fd5232e'
+
