366d1aae2dba2d726293a140203c02364e3e73d9
Viktor Toth <viktorrrrrrrr@rrrrrrr.hu>
2019-04-09 14:26:37 +0200

    console print fix

--- Ripple/run_on_mainnet.sh
+++ Ripple/run_on_mainnet.sh
@@ -8,7 +8,7 @@ if [ ! -f ${SCRIPTPATH}/config/rippled.cfg ] ; then
 	echo "Copy sample config into ./config/rippled.cfg, because it wasn't exist"
 	cp ${SCRIPTPATH}/rippled-${NET}-sample.cfg ${SCRIPTPATH}/config/rippled.cfg
 else
-	read -p "The configuration already set in the ./config/rippled.cfg. Do you want to replace it by the working sample testnet configuration? (y or n): " yn
+	read -p "The configuration already set in the ./config/rippled.cfg. Do you want to replace it by the working sample ${NET} configuration? (y or n): " yn
 	while true; do
 		case $yn in
 			[Yy]* ) cp ${SCRIPTPATH}/rippled-${NET}-sample.cfg ${SCRIPTPATH}/config/rippled.cfg; break;;
