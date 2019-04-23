95ca0990981091755548dcfc4553738ad738e605
Viktor <rnreng42@gmail.com>
2019-04-23 14:45:29 +0000

    add ports to cointainer

--- Ripple/run_on_mainnet.sh
+++ Ripple/run_on_mainnet.sh
@@ -8,7 +8,7 @@ if [ ! -f ${SCRIPTPATH}/config/rippled.cfg ] ; then
 	echo "Copy sample config into ./config/rippled.cfg, because it wasn't exist"
 	cp ${SCRIPTPATH}/rippled-${NET}-sample.cfg ${SCRIPTPATH}/config/rippled.cfg
 else
-	read -p "The configuration already set in the ./config/rippled.cfg. Do you want to replace it by the working sample testnet configuration? (y or n): " yn
+	read -p "The configuration already set in the ./config/rippled.cfg. Do you want to replace it by the working sample ${NET} configuration? (y or n): " yn
 	while true; do
 		case $yn in
 			[Yy]* ) cp ${SCRIPTPATH}/rippled-${NET}-sample.cfg ${SCRIPTPATH}/config/rippled.cfg; break;;
@@ -21,7 +21,7 @@ if [ ! -f ${SCRIPTPATH}/config/validators.txt ] ; then
         echo "Copy sample config into ./config/validator.txt, because it wasn't exist"
         cp ${SCRIPTPATH}/validators-${NET}-sample.txt ${SCRIPTPATH}/config/validators.txt
 else
-        read -p "The configuration already set in the ./config/rippled.cfg. Do you want to replace it by the working sample testnet configuration? (y or n): " yn
+        read -p "The configuration already set in the ./config/rippled.cfg. Do you want to replace it by the working sample ${NET} configuration? (y or n): " yn
         while true; do
                 case $yn in
                         [Yy]* ) cp ${SCRIPTPATH}/validators-${NET}-sample.txt ${SCRIPTPATH}/config/validators.txt; break;;
@@ -30,4 +30,4 @@ else
                 esac
         done
 fi
-docker run -d -v ${SCRIPTPATH}/config:/home/cointainer/.config/ripple --name ripple-node-${NET} cointainer/ripple:latest $@
+docker run -d -p 127.0.0.1:5005:5005 -p 127.0.0.1:6006:6006 -v ${SCRIPTPATH}/config:/home/cointainer/.config/ripple --name ripple-node-${NET} cointainer/ripple:latest $@
